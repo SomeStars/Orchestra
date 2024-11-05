c36bf417778326273f89efef22df3dbb431c0166
Gabe Roberts <gsroberts@uncg.edu>
2024-11-05 17:18:44 -0500

    initial commit

---
+++ orchestra/src/test/java/CSC_340_01/orchestra/OrchestraApplicationTests.java
@@ -0,0 +1,13 @@
+package CSC_340_01.orchestra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
