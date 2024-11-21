b303ebe7ea2255263b212ee68e263ae2d137d1a9
Gabe Roberts <gsroberts@uncg.edu>
2024-11-21 13:37:11 -0500

    edits

--- orchestra/src/main/java/CSC_340_01/orchestra/Controller/ProviderController.java
+++ orchestra/src/main/java/CSC_340_01/orchestra/Controller/ProviderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/providers")
+@RequestMapping("/providers")
 public class ProviderController {
     private final ProviderService providerService;
 
