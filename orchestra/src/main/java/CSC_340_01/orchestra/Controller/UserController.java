67d2d03e7f198a5cbfc1557576e99ac6c9e68225
matthewingallinera <122546606+matthewingallinera@users.noreply.github.com>
2024-12-10 17:09:28 -0500

    Added login functionality that connects to the mysql database and added create account functionality.

--- orchestra/src/main/java/CSC_340_01/orchestra/Controller/UserController.java
+++ orchestra/src/main/java/CSC_340_01/orchestra/Controller/UserController.java
@@ -1,27 +1,106 @@
 package CSC_340_01.orchestra.Controller;
 
 import CSC_340_01.orchestra.Model.User;
+import CSC_340_01.orchestra.Repository.UserRepository;
 import CSC_340_01.orchestra.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.swing.text.html.Option;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
 
     private final UserService userService;
+
+    private AuthenticationManager authenticationManager;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
+
+    @PostMapping("/index")
+    public String showIndex(){
+        return "index";
+    }
+
+    @PostMapping("/userLogin")
+    public String userLogin(@RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes) {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            if (user.getPassword().equals(password)) {
+                return "index";
+            } else {
+                redirectAttributes.addFlashAttribute("error", "Wrong Credentials");
+                return "login";
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Not a user");
+            return "login";
+
+        }
+    }
+
+    @GetMapping("/create")
+    public String showCreateUserPage(){
+        return "createUser";
+    }
+
+    @PostMapping("/create-login")
+    public String createUser(@RequestParam String email, @RequestParam String role, @RequestParam String username, @RequestParam String password, @RequestParam String confirmPassword, RedirectAttributes redirectAttributes){
+        Optional<User> emailExists = userRepository.findByEmail(email);
+        if (emailExists.isPresent()){
+            redirectAttributes.addFlashAttribute("error", "email already exists");
+            return "create";
+        }
+
+        if (!password.equals(confirmPassword)){
+            redirectAttributes.addFlashAttribute("error", "passwords are different. Try again.");
+            return "create";
+        }
+
+        Optional<User> usernameExists = userRepository.findByUsername(username);
+        if (usernameExists.isPresent()){
+            redirectAttributes.addFlashAttribute("error", "Username already exists. Pick another Username.");
+            return "create";
+        }
+
+        User newAccount = new User();
+        newAccount.setUsername(username);
+        newAccount.setPassword(password);
+        newAccount.setEmail(email);
+        newAccount.setRole(role);
+        userRepository.save(newAccount);
+
+        return "login";
+    }
 
     @GetMapping("/all")
     public String getAllUsers(Model model) {
@@ -30,7 +109,7 @@ public class UserController {
         return "manage-users";
     }
 
-    /*
+
     @GetMapping("/{userId}")
     public User getOneUser(@PathVariable long userId){
         return userService.getUserById(userId);
@@ -48,7 +127,7 @@ public class UserController {
         userService.updateUser(userId, user);
         return userService.getUserById(userId);
     }
-    */
+
 
 
 }
--- orchestra/src/main/java/CSC_340_01/orchestra/Repository/UserRepository.java
+++ orchestra/src/main/java/CSC_340_01/orchestra/Repository/UserRepository.java
@@ -3,6 +3,10 @@ package CSC_340_01.orchestra.Repository;
 import CSC_340_01.orchestra.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
